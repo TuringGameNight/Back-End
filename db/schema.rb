071263576fe9d6dcdb677537c10cfd62ea557e9a
Shaun James <ShaunDaneJames@Gmail.com>
2021-01-06 15:46:17 -0700

    Adds user reference to games
    Co-authored-by: Kate Tester <5349599+katemorris@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_222525) do
+ActiveRecord::Schema.define(version: 2021_01_06_224444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2021_01_06_222525) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_games_on_user_id"
   end
 
   create_table "invitations", force: :cascade do |t|
@@ -78,6 +80,7 @@ ActiveRecord::Schema.define(version: 2021_01_06_222525) do
 
   add_foreign_key "friends", "users"
   add_foreign_key "game_nights", "users"
+  add_foreign_key "games", "users"
   add_foreign_key "invitations", "game_nights"
   add_foreign_key "invitations", "users"
   add_foreign_key "user_games", "games"
