9df79544efcd920bee6a0228b4baf5a72a6f2439
Shaun James <ShaunDaneJames@Gmail.com>
2021-01-06 15:58:07 -0700

    Adds user relationships and validations
    Co-authored-by: Kate Tester <5349599+katemorris@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+
+  validates :email, uniqueness: true
+  validates :name, :email, presence: { require: true }
+  validates :google_id, :google_token, :refresh_token, :auth_token, presence: { require: true }
+
+  has_many :game_nights, dependent: :destroy
+  has_many :invitations
+  has_many :friends, dependent: :destroy
+  has_many :buds, through: :friends
+  has_many :games
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+  describe "validations" do
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :google_id }
+    it { should validate_presence_of :google_token }
+    it { should validate_presence_of :refresh_token }
+    it { should validate_presence_of :auth_token }
+  end
+
+  describe "relationships" do
+    it { should have_many :game_nights }
+    it { should have_many :friends}
+    it { should have_many(:buds).through :friends}
+    it { should have_many :games}
+    it { should have_many :invitations}
+  end
 end
