9761825e0b222f8746e6e280881abdf8e49bfcdd
Shaun James <ShaunDaneJames@Gmail.com>
2021-01-06 16:39:43 -0700

    Adds validations and relationships for game
    Co-authored-by: Kate Tester <5349599+katemorris@users.noreply.github.com>

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Game, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name}
+    it { should validate_presence_of :type}
+    it { should validate_presence_of :description}
+    it { should validate_presence_of :age_range}
+    it { should validate_presence_of :duration}
+    it { should validate_presence_of :user_id}
+  end
+
+  describe 'relationships' do
+    it { should belong_to :user}
+  end
 end
