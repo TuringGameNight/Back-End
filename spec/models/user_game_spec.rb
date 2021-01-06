31650f80c8f0486940b05eb8a0c33d9aa28ab8fb
Shaun James <ShaunDaneJames@Gmail.com>
2021-01-06 16:51:14 -0700

    Adds validations and relationships to usergame
    Co-authored-by: Kate Tester <5349599+katemorris@users.noreply.github.com>

--- spec/models/user_game_spec.rb
+++ spec/models/user_game_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe UserGame, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe UserGame, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :game_id}
+    it { should validate_presence_of :user_id}
+  end
+
+  describe 'relationships' do
+    it { should belong_to :user}
+    it { should belong_to :game}
+  end
 end
