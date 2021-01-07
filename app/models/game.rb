3208587b55c1a6dc0a6f7a781f7aee15cbabc843
Shaun James <ShaunDaneJames@Gmail.com>
2021-01-07 16:59:09 -0700

    Updates game model test and table to have num_players
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,9 +2,10 @@ class Game < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
-  validates :type, presence: true
+  validates :game_type, presence: true
   validates :description, presence: true
   validates :age_range, presence: true
   validates :duration, presence: true
   validates :user_id, presence: true
+  validates :num_players, presence: true
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 describe Game, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name}
-    it { should validate_presence_of :type}
+    it { should validate_presence_of :game_type}
     it { should validate_presence_of :description}
     it { should validate_presence_of :age_range}
     it { should validate_presence_of :duration}
     it { should validate_presence_of :user_id}
+    it { should validate_presence_of :num_players }
   end
 
   describe 'relationships' do
