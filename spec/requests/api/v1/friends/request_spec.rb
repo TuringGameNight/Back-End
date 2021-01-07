be707bc041382fb2eefa477ba7e3c3722269bea2
Shaun James <ShaunDaneJames@Gmail.com>
2021-01-06 20:24:56 -0700

    Adds test for friends endpoint
    Co-authored-by: Kate Tester <5349599+katemorris@users.noreply.github.com>

---
+++ spec/requests/api/v1/friends/request_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'Friends endpoint', type: :request do
+  it 'sends all friends data for a particular user' do
+    user_1 = create(:user)
+    user_2 = create(:user)
+    user_3 = create(:user)
+    Friend.create(user_id:user_1.id, bud_id:user_2.id)
+    Friend.create(user_id:user_1.id, bud_id:user_3.id)
+
+    get api_v1_user_friends_path(user_1.id)
+
+    expect(response).to be_successful
+
+    friends = JSON.parse(response.body, symbolize_names: true)[:data]
+    friends.each do |friend|
+      expect(friend).to have_key(:id)
+      expect(friend[:id]).to be_an(String)
+      expect(friend[:attributes]).to have_key(:name)
+      expect(friend[:attributes][:name]).to be_a(String)
+      expect(friend[:attributes]).to have_key(:email)
+      expect(friend[:attributes][:email]).to be_a(String)
+    end
+  end
+end
