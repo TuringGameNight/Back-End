ac33416860cc658f9917e7a5f8fed19c79e6d565
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-07 19:06:38 -0700

    Adds create_user_spec
    Co-authored-by: Shaunda Cunningham 
    <4582791+smcunning@users.noreply.github.com>

---
+++ spec/requests/api/v1/users/create_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Create User' do
+  it "can create a new user" do
+    user_request_info = File.read('spec/fixtures/user_oauth_data.rb')
+    headers = {'CONTENT_TYPE' => 'application/json'}
+    post '/api/v1/users', params: user_request_info
+    created_user = User.last
+
+    expect(response).to be_successful
+    expect(created_user.name).to eq(user_request_info[:user_data][:info][:name])
+  end
+
+end
