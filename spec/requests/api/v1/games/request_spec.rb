e6bdc5c52f42c35340db727d8a394184d6782c5a
Shaun James <ShaunDaneJames@Gmail.com>
2021-01-07 17:36:50 -0700

    Adds test for add game endpoint
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>

---
+++ spec/requests/api/v1/games/request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'Add game endpoint', type: :request do
+  it 'Allows user to add a game not discovered by the API' do
+    user_1 = create(:user)
+
+    post api_v1_user_games_path(user_1.id), params: {name: 'DnD',
+                                                     game_type: "Dice",
+                                                     num_players: "4-8",
+                                                     description: "Fun times!",
+                                                     age_range: "10",
+                                                     duration: 2,
+                                                     image: "N/A",
+                                                     user_id: user_1.id}
+
+    expect(Game.last.name).to eq('DnD')
+  end
+end
