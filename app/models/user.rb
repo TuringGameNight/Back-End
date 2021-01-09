d095df1b9fd4a1085fd4a7b621095eab470beecf
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-09 13:51:59 -0700

    removes refresh token from user model
    Co-authored-by: Shaunda Cunningham 
    <4582791+smcunning@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true
   validates :name, :email, presence: { require: true }
-  validates :google_id, :google_token, :refresh_token, :auth_token, presence: { require: true }
+  validates :google_id, :google_token, :auth_token, presence: { require: true }
 
   has_many :game_nights, dependent: :destroy
   has_many :invitations
@@ -12,13 +12,12 @@ class User < ApplicationRecord
 
   def self.find_or_update(user_data)
     user = User.find_by(email: user_data[:user_data][:info][:email]) || User.create!(
-      google_id: user_params[:client_id],
-         name: user_params[:user_data][:info][:name],
-          image: user_params[:user_data][:info][:image],
-           auth_token: user_params[:user_data][:credentials][:token],
-            refresh_token: user_params[:user_data][:credentials][:refresh_token],
-             google_token: user_params[:user_data][:extra][:id_token],
-              email: user_params[:user_data][:info][:email]
+      google_id: user_data[:client_id],
+         name: user_data[:user_data][:info][:name],
+          image: user_data[:user_data][:info][:image],
+           auth_token: user_data[:user_data][:credentials][:token],
+             google_token: user_data[:user_data][:extra][:id_token],
+              email: user_data[:user_data][:info][:email]
     )
     user.save
     user
