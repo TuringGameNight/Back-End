39fc336885b0f3f927d5a162ba6a6b28ae3fc77f
Kate Tester <katemorris@gmail.com>
2021-01-09 15:48:44 -0700

    Modifies controller to check db and then request API

--- app/controllers/api/v1/games/find_controller.rb
+++ app/controllers/api/v1/games/find_controller.rb
@@ -1,15 +1,11 @@
 class Api::V1::Games::FindController < ApplicationController
 
   def index
-    game_name = params[:format]
-    if find_game_db(game_name).empty?
-      p 'do the sinatra api call'
+    game_name = params[:name]
+    if Game.find_game_db(game_name).empty?
+      render json: SearchFacade.find_games_by_name(game_name)
     else
-    render json: GamesSerializer.new(find_game_db(game_name))
+      render json: GamesSerializer.new(Game.find_game_db(game_name))
     end
   end
-
-  private
-
-  
 end
