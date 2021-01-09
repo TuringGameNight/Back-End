e8aad9d5ec4e469a434c74843e6bb66682e6a18e
Kate Tester <katemorris@gmail.com>
2021-01-09 11:31:43 -0700

    Moves search of game in DB to game model.

--- app/controllers/api/v1/games/find_controller.rb
+++ app/controllers/api/v1/games/find_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::Games::FindController < ApplicationController
 
-  def show
-    if Game.where("lower(name) LIKE ?", "%" + params[:format].downcase + "%") == []
+  def index
+    game_name = params[:format]
+    if find_game_db(game_name).empty?
       p 'do the sinatra api call'
     else
-    render json: GamesSerializer.new(
-      Game.where("lower(name) LIKE ?", "%" + params[:format].downcase + "%"))
+    render json: GamesSerializer.new(find_game_db(game_name))
     end
   end
+
+  private
+
+  
 end
