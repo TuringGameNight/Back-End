4bf8d31198c8017ac8e7cbb7254dde2b05af674a
Shaun James <ShaunDaneJames@Gmail.com>
2021-01-08 21:07:29 -0700

    Adds route for game find

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # Friends
   namespace :api do
     namespace :v1 do
 
+      # Games lookup
+      namespace :games do
+        get '/find', to: 'find#show'
+      end
+
+      # User crud
       resources :users do
         get :friends, to: 'users/friends#index'
       end
 
+      # Games crud
       resources :games do
         post :games, to: 'games/games#create'
       end
     end
   end
 end
-
-# The follow was tried but gives too much crud functionality for a user
-# Games
-# namespace :api do
-#   namespace :v1 do
-#     resources :users do
-#       get :games, to: 'users/games#new'
-#       post :games, to: 'users/games#create'
-#     end
-#   end
-# end
