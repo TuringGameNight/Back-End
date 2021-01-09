39902b698347880b61555c9cd62f6f61c47343f7
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-08 18:53:05 -0700

    Adds users_controller with create action
    Co-authored-by: Shaunda Cunningham 
    <4582791+smcunning@users.noreply.github.com>

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    binding.pry
-    user_params = JSON.parse(params, symbolize_names: true)
-    user = User.find_or_create_by()
+    user_params = JSON.parse(request.body.read, symbolize_names: true)
+
+    user = User.find_or_create_by(google_id: user_params[:client_id],
+       name: user_params[:user_data][:info][:name],
+        image: user_params[:user_data][:info][:image],
+         auth_token: user_params[:user_data][:credentials][:token],
+          refresh_token: user_params[:user_data][:credentials][:refresh_token],
+           google_token: user_params[:user_data][:extra][:id_token],
+            email: user_params[:user_data][:info][:email])
+    render json: UserSerializer.new(user)
   end
 
   private
