f81c2153753daea2bce215cebf5292b99fccad8c
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-08 19:47:18 -0700

    Edits create action in users controller
    Co-authored-by: Shaunda Cunningham 
    <4582791+smcunning@users.noreply.github.com>

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,21 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def create
     user_params = JSON.parse(request.body.read, symbolize_names: true)
-
-    user = User.find_or_create_by(google_id: user_params[:client_id],
-       name: user_params[:user_data][:info][:name],
-        image: user_params[:user_data][:info][:image],
-         auth_token: user_params[:user_data][:credentials][:token],
-          refresh_token: user_params[:user_data][:credentials][:refresh_token],
-           google_token: user_params[:user_data][:extra][:id_token],
-            email: user_params[:user_data][:info][:email])
+    user = User.find_or_update(user_params)
     render json: UserSerializer.new(user)
   end
-
-  private
-
-  # def user_params
-  #   params.permit()
-  # end
-
 end
