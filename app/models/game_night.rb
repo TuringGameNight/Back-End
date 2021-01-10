b728c6b2b54784af89912490d512dc4571f2bbaf
Kate Tester <katemorris@gmail.com>
2021-01-10 14:50:07 -0700

    Working method to get games from attendees of game night

--- app/models/game_night.rb
+++ app/models/game_night.rb
@@ -8,33 +8,16 @@ class GameNight < ApplicationRecord
   validates :number_of_games, presence: true
   validates :name, presence: true
 
-  # def games_to_play
-  #   Game.
-  #   joins(users: [:invitations, :game_nights]).
-  #   where('invitations.game_night_id = ? OR game_nights.id = ?', self.id, self.id).
-  #   distinct
-  #
-  # end
-  #
-  # Game.
-  # joins(users: [:invitations, :game_nights]).
-  # where('invitations.game_night_id = ? OR game_nights.id = ?', self.id, self.id).
-  # distinct
-  # #
-  # def method_name
-  #   select games.* from games
-  #   inner join user_games on games.id = user_games.game_id
-  #   inner join users on user_games.user_id = user.id
-  #   inner join invitations on invitations.user_id = user.id
-  #   inner join game_nights on game_nights.user_id = user.id
-  #   where invitations.game_night_id = self.id OR game_night.id = self.id
-  # end
-
-  # def attendee_games(users)
-  #   games = []
-  #   users.each do |user|
-  #     games << user.games
-  #   end
-  #   games
-  # end
+  def games_to_play
+    # I welcome optimizations here. But this took a WHILE.
+    attendees = User.
+                joins(:invitations).
+                where('invitations.game_night_id = ?', self.id).
+                pluck(:id)
+    users = attendees + User.
+                        joins(:game_nights).
+                        where('game_nights.id = ?', self.id).
+                        pluck(:id)
+    Game.joins(:users).where(User.arel_table[:id].in(users)).distinct
+  end
 end
