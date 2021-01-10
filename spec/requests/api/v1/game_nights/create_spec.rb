a7157af706344802610e856a2128ceb5bf3fafe2
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-09 19:19:23 -0700

    Adds game_nights create spec to requests

---
+++ spec/requests/api/v1/game_nights/create_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Create Game Night' do
+  it "creates game night from request and responds with appropriate data" do
+    user_1 = create :user
+    user_2 = create :user
+
+    Friend.create(user_id: user_1.id, bud_id: user_2.id)
+    Friend.create(user_id: user_2.id, bud_id: user_1.id)
+
+    game_1 = create :game
+    game_2 = create :game
+
+    UserGame.create(user_id: user_1.id, game_id: game_1.id)
+    UserGame.create(user_id: user_2.id, game_id: game_2.id)
+
+    game_night_params = {
+      :user_id=>user_1.id,
+      :name=>'Friday Fun Night',
+      :date=>'1/15/2021',
+      :number_of_games=>2,
+      :
+    }
+
+    post '/api/v1/game_nights', headers: headers, params: JSON.generate(game_night_params)
+
+    game_night = JSON.parse(response.body, symbolize_names: true)
+
+    binding.pry
+  end
+end
