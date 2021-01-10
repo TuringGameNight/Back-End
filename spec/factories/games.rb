dd943bb996fb1841df67bad677a86757d2ccc9cd
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-09 19:19:37 -0700

    edits games factory

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
   factory :game do
-    name { "MyString" }
-    type { "" }
-    description { "MyString" }
-    age_range { "MyString" }
-    duration { 1 }
-    image { "MyString" }
+    name { Faker::Game.title }
+    game_type { Faker::Game.genre }
+    description { Faker::Lorem.sentence }
+    age_range { "1 - 5000" }
+    duration { 60 }
+    image { Faker::LoremFlickr.image }
+    num_players { "4" }
   end
 end
