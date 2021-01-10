da2a56c4cf583d3c5779a8c962ead32f91bd89e3
Kate Tester <katemorris@gmail.com>
2021-01-09 21:07:17 -0700

    WIP - games to play, have not gotten it right yet

--- app/models/game_night.rb
+++ app/models/game_night.rb
@@ -1,8 +1,22 @@
 class GameNight < ApplicationRecord
   belongs_to :user
+  has_many :invitations
+  has_many :users, through: :invitations
 
-    validates :user_id, presence: true
-    validates :date, presence: true
-    validates :number_of_games, presence: true
-    validates :name, presence: true
+  validates :user_id, presence: true
+  validates :date, presence: true
+  validates :number_of_games, presence: true
+  validates :name, presence: true
+
+  def games_to_play
+    (attendee_games(self.users) + self.user.games).uniq
+  end
+
+  def attendee_games(users)
+    games = []
+    users.each do |user|
+      games << user.games
+    end
+    games
+  end
 end
--- spec/models/game_night_spec.rb
+++ spec/models/game_night_spec.rb
@@ -2,13 +2,43 @@ require 'rails_helper'
 
 describe GameNight, type: :model do
   describe 'validations' do
-    it { should validate_presence_of :name}
-    it { should validate_presence_of :date}
-    it { should validate_presence_of :number_of_games}
-    it { should validate_presence_of :user_id}
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :date }
+    it { should validate_presence_of :number_of_games }
+    it { should validate_presence_of :user_id }
   end
 
   describe 'relationships' do
-    it { should belong_to :user}
+    it { should belong_to :user }
+    it { should have_many :invitations }
+    it { should have_many(:users).through(:invitations) }
+  end
+
+  describe 'instance methods' do
+    xit '#games_to_play' do
+      user_1 = create :user
+      user_2 = create :user
+
+      Friend.create(user_id: user_1.id, bud_id: user_2.id)
+      Friend.create(user_id: user_2.id, bud_id: user_1.id)
+
+      game_1 = create :game
+      game_2 = create :game
+
+      UserGame.create(user_id: user_1.id, game_id: game_1.id)
+      UserGame.create(user_id: user_1.id, game_id: game_2.id)
+      UserGame.create(user_id: user_2.id, game_id: game_2.id)
+
+      gn = GameNight.create!(
+        user_id: user_1.id,
+        name:'Friday Fun Night',
+        date: '1/15/2021',
+        number_of_games: 2)
+
+      Invitation.create!(user_id: user_2.id, game_night_id: gn.id, status: 'pending')
+
+      expect(gn.games_to_play).to eq([game_1, game_2])
+      expect(gn.games_to_play).to_not eq([game_1, game_2, game_2])
+    end
   end
 end
