ad9d84a9c48c0141ff2db4bf2a60afa116ec1e7c
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-12 13:42:16 -0700

    normal case for variable numbers as per hound
    Co-authored-by: Shaun James 
    <58994078+ShaunDaneJames@users.noreply.github.com>

--- spec/models/game_night_spec.rb
+++ spec/models/game_night_spec.rb
@@ -18,48 +18,48 @@ describe GameNight, type: :model do
 
   describe 'instance methods' do
     before do
-      @user_1 = create :user
-      @user_2 = create :user
-      @user_3 = create :user
+      @user1 = create :user
+      @user2 = create :user
+      @user3 = create :user
 
-      Friend.create(user_id: @user_1.id, bud_id: @user_2.id)
-      Friend.create(user_id: @user_2.id, bud_id: @user_1.id)
-      Friend.create(user_id: @user_1.id, bud_id: @user_3.id)
-      Friend.create(user_id: @user_3.id, bud_id: @user_1.id)
+      Friend.create(user_id: @user1.id, bud_id: @user2.id)
+      Friend.create(user_id: @user2.id, bud_id: @user1.id)
+      Friend.create(user_id: @user1.id, bud_id: @user3.id)
+      Friend.create(user_id: @user3.id, bud_id: @user1.id)
 
-      @game_1 = create :game
-      @game_2 = create :game
+      @game1 = create :game
+      @game2 = create :game
 
-      UserGame.create(user_id: @user_1.id, game_id: @game_1.id)
-      UserGame.create(user_id: @user_1.id, game_id: @game_2.id)
-      UserGame.create(user_id: @user_2.id, game_id: @game_2.id)
+      UserGame.create(user_id: @user1.id, game_id: @game1.id)
+      UserGame.create(user_id: @user1.id, game_id: @game2.id)
+      UserGame.create(user_id: @user2.id, game_id: @game2.id)
 
       @gn = GameNight.create!(
-        user_id: @user_1.id,
+        user_id: @user1.id,
         name: 'Friday Fun Night',
         date: '1/15/2021',
         number_of_games: 2
       )
 
-      Invitation.create!(user_id: @user_2.id, game_night_id: @gn.id, status: 'accepted')
+      Invitation.create!(user_id: @user2.id, game_night_id: @gn.id, status: 'accepted')
       Invitation.create!(
         status: 'pending',
-        user_id: @user_3.id,
+        user_id: @user3.id,
         game_night_id: @gn.id
       )
     end
 
     it '#games_to_play' do
-      expect(@gn.games_to_play.sort).to eq([@game_1, @game_2].sort)
-      expect(@gn.games_to_play.sort).not_to eq([@game_1, @game_2, @game_2].sort)
+      expect(@gn.games_to_play.sort).to eq([@game1, @game2].sort)
+      expect(@gn.games_to_play.sort).not_to eq([@game1, @game2, @game2].sort)
     end
 
     it '#confirmed_attendees' do
-      expect(@gn.confirmed_attendees.sort).to eq([@user_1, @user_2].sort)
+      expect(@gn.confirmed_attendees.sort).to eq([@user1, @user2].sort)
     end
 
     it '#pending_attendees' do
-      expect(@gn.pending_attendees.sort).to eq([@user_3].sort)
+      expect(@gn.pending_attendees.sort).to eq([@user3].sort)
     end
   end
 end
