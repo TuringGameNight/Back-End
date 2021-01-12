d71ba5e273a8d42733ef7f6cc2b7d9fc0e716ad6
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-12 13:00:32 -0600

    Add error handling to api/v1/user/games POST and DELETE

--- spec/requests/api/v1/games/request_spec.rb
+++ spec/requests/api/v1/games/request_spec.rb
@@ -17,6 +17,12 @@ RSpec.describe 'Api/V1/Games', type: :request do
         duration: 2,
         image: 'N/A'
       }
+      
+      json_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(200)
+      expect(json_body).to have_key(:message)
+      expect(json_body[:message]).to eq('success')
 
       expect(Game.last.name).to eq('DnD')
       expect(UserGame.last.user_id).to eq(user.id)
@@ -36,6 +42,25 @@ RSpec.describe 'Api/V1/Games', type: :request do
       expect(Game.last.name).to eq('Catan')
       expect(UserGame.last.user_id).to eq(user.id)
       expect(UserGame.last.game_id).to eq(Game.last.id)
+    end
+
+    context 'when creation in unsuccessful' do
+      it 'returns a json error message' do
+        user = create(:user)
+
+        post api_v1_games_path, params: {
+          user_id: user.id,
+          name: 'DnD',
+        }
+
+        json_body = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(422)
+        expect(json_body).to have_key(:message)
+        expect(json_body[:message]).to eq('unsuccessful')
+        expect(json_body).to have_key(:error)
+        expect(json_body[:error]).to eq("Validation failed: Description can't be blank, Duration can't be blank")
+      end
     end
   end
 end
