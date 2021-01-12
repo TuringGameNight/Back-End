862e55b47307c4d18c9e2bbc798191d100ca789b
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-12 15:17:30 -0700

    Edits show spec to use updated serializers
    Co-authored-by: Shaun James 
    <58994078+ShaunDaneJames@users.noreply.github.com>

--- spec/requests/api/v1/game_nights/show_spec.rb
+++ spec/requests/api/v1/game_nights/show_spec.rb
@@ -38,24 +38,23 @@ RSpec.describe 'GET Game Night' do
     get "/api/v1/game_nights/#{game_night.id}"
 
     game_night_data = JSON.parse(response.body, symbolize_names: true)
-
     game_night_show_response_checker(game_night_data, game_night, user1)
     expect(game_night_data[:data][:attributes][:games].count).to eq(2)
     expect(game_night_data[:data][:attributes][:confirmed_attendees].count).to eq(2)
-    expect(game_night_data[:data][:attributes][:confirmed_attendees].first[:data][:id]).to eq(user2.id.to_s)
-    expect(game_night_data[:data][:attributes][:confirmed_attendees].first[:data][:attributes][:name]).to eq(user2.name)
-    expect(game_night_data[:data][:attributes][:confirmed_attendees].first[:data][:attributes][:email]).to eq(user2.email)
-    expect(game_night_data[:data][:attributes][:confirmed_attendees].second[:data][:id]).to eq(user1.id.to_s)
-    expect(game_night_data[:data][:attributes][:confirmed_attendees].second[:data][:attributes][:name]).to eq(user1.name)
-    expect(game_night_data[:data][:attributes][:confirmed_attendees].second[:data][:attributes][:email]).to eq(user1.email)
+    expect(game_night_data[:data][:attributes][:confirmed_attendees].first[:id]).to eq(user2.id)
+    expect(game_night_data[:data][:attributes][:confirmed_attendees].first[:name]).to eq(user2.name)
+    expect(game_night_data[:data][:attributes][:confirmed_attendees].first[:email]).to eq(user2.email)
+    expect(game_night_data[:data][:attributes][:confirmed_attendees].second[:id]).to eq(user1.id)
+    expect(game_night_data[:data][:attributes][:confirmed_attendees].second[:name]).to eq(user1.name)
+    expect(game_night_data[:data][:attributes][:confirmed_attendees].second[:email]).to eq(user1.email)
     expect(game_night_data[:data][:attributes][:confirmed_attendees].first).not_to have_key(:google_token)
     expect(game_night_data[:data][:attributes][:confirmed_attendees].first).not_to have_key(:auth_token)
     expect(game_night_data[:data][:attributes][:confirmed_attendees].first).not_to have_key(:refresh_token)
     expect(game_night_data[:data][:attributes][:confirmed_attendees].first).not_to have_key(:google_id)
     expect(game_night_data[:data][:attributes][:pending_attendees].count).to eq(1)
-    expect(game_night_data[:data][:attributes][:pending_attendees].first[:data][:id]).to eq(user3.id.to_s)
-    expect(game_night_data[:data][:attributes][:pending_attendees].first[:data][:attributes][:name]).to eq(user3.name)
-    expect(game_night_data[:data][:attributes][:pending_attendees].first[:data][:attributes][:email]).to eq(user3.email)
+    expect(game_night_data[:data][:attributes][:pending_attendees].first[:id]).to eq(user3.id)
+    expect(game_night_data[:data][:attributes][:pending_attendees].first[:name]).to eq(user3.name)
+    expect(game_night_data[:data][:attributes][:pending_attendees].first[:email]).to eq(user3.email)
   end
 
   it 'gets the data about a game night that has no invitations' do
