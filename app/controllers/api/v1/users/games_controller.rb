0ae1830b67f9c34bf26b4f5c7d89c45a0a9e4c96
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-12 11:06:24 -0600

    Cleanup trailing whitespace

--- app/controllers/api/v1/users/games_controller.rb
+++ app/controllers/api/v1/users/games_controller.rb
@@ -6,7 +6,7 @@ module Api
       class GamesController < ApplicationController
         def index
           user_games = User.find(params[:user_id]).games
-          
+
           render json: GamesSerializer.new(user_games)
         end
 
