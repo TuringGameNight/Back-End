db9ba322ceb656086ee73f7409051f014144edb1
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-12 11:23:19 -0700

    Edits game_night model spec to test new confirmed_attendees and pending_attendees methods
    Co-authored-by: Shaun James 
    <58994078+ShaunDaneJames@users.noreply.github.com>

--- spec/models/game_night_spec.rb
+++ spec/models/game_night_spec.rb
@@ -20,9 +20,12 @@ describe GameNight, type: :model do
     before do
       @user_1 = create :user
       @user_2 = create :user
+      @user_3 = create :user
 
       Friend.create(user_id: @user_1.id, bud_id: @user_2.id)
       Friend.create(user_id: @user_2.id, bud_id: @user_1.id)
+      Friend.create(user_id: @user_1.id, bud_id: @user_3.id)
+      Friend.create(user_id: @user_3.id, bud_id: @user_1.id)
 
       @game_1 = create :game
       @game_2 = create :game
@@ -38,7 +41,12 @@ describe GameNight, type: :model do
         number_of_games: 2
       )
 
-      Invitation.create!(user_id: @user_2.id, game_night_id: @gn.id, status: 'pending')
+      Invitation.create!(user_id: @user_2.id, game_night_id: @gn.id, status: 'accepted')
+      Invitation.create!(
+        status: 'pending',
+        user_id: @user_3.id,
+        game_night_id: @gn.id
+      )
     end
 
     it '#games_to_play' do
@@ -46,8 +54,12 @@ describe GameNight, type: :model do
       expect(@gn.games_to_play.sort).not_to eq([@game_1, @game_2, @game_2].sort)
     end
 
-    it '#attendees' do
-      expect(@gn.attendees.sort).to eq([@user_1, @user_2].sort)
+    it '#confirmed_attendees' do
+      expect(@gn.confirmed_attendees.sort).to eq([@user_1, @user_2].sort)
+    end
+
+    it '#pending_attendees' do
+      expect(@gn.pending_attendees.sort).to eq([@user_3].sort)
     end
   end
 end
