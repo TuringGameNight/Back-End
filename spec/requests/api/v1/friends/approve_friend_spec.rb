8c7b137e899db74b98e4c795ce800cdc59721c3b
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-12 16:45:28 -0700

    WIP spec for friends approval
    Co-authored-by: Grant Dempsey 34411972+GDemps@users.noreply.github.com

--- spec/requests/api/v1/friends/approve_friend_spec.rb
+++ spec/requests/api/v1/friends/approve_friend_spec.rb
@@ -4,28 +4,27 @@ describe 'Friend request accept/decline', type: :request do
   it 'Can receive request with option to accept or decline request' do
     user_1 = create(:user)
     user_2 = create(:user)
-    friendship = Friend.create!(user_id: user_1.id, bud_id: user_2.id)
+    user_3 = create(:user)
 
-    # input = {"data"=>[{"id"=>"#{user_1.id}", "type"=>"friends", "attributes"=>{"name"=>"Hades",
-    #           "email"=>"idella@dubuque.org"}}], "controller"=>"
-    #           api/v1/users/friends", "action"=>"update", "id"=>"#{user_2.id}", "status"=>"accepted"}
+    friendship = Friend.create!(user_id: user_1.id, bud_id: user_2.id)
+    friendship_2 = Friend.create!(user_id: user_1.id, bud_id: user_3.id)
 
     expect(Friend.last.status).to eq('pending')
 
     get api_v1_user_friends_path(user_1.id)
 
     expect(response).to be_successful
-    headers = {"CONTENT_TYPE" => "application/json"}
+
     friends_data = JSON.parse(response.body, symbolize_names: true)
     friends = friends_data[:data].first
-    require "pry"
-    binding.pry
-    patch "/api/v1/users/#{user_1.id}/friends/#{friends[:id]}", headers: headers, params: friends_data
+    friends[:attributes][:status] = "accepted"
+
+    patch "/api/v1/users/#{user_1.id}/friends/#{friends[:id]}", headers: headers, params: JSON.generate(friends)
 
     friends_data = JSON.parse(response.body, symbolize_names: true)
     friends = friends_data[:data].first
 
     expect(response).to be_successful
-    expect(friends[:status]).to eq('approved')
+    expect(friends[:status]).to eq('accepted')
   end
 end
