3a248029d84ae09d0f311675e3966f09220c3f42
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-12 18:46:10 -0700

    rubocop auto correct

--- spec/response_checkers.rb
+++ spec/response_checkers.rb
@@ -73,10 +73,10 @@ module ResponseCheckers
     expect(user_gn_response[:data].first[:attributes][:number_of_games]).to eq(gn_1.number_of_games)
   end
 
-  def game_night_show_response_checker(game_night_data, game_night, user)
+  def game_night_show_response_checker(game_night_data, game_night)
     expect(game_night_data[:data]).to have_key(:id)
     expect(game_night_data[:data][:id]).to be_an(String)
-
+  
     expect(game_night_data[:data][:attributes]).to have_key(:name)
     expect(game_night_data[:data][:attributes][:name]).to be_an(String)
     expect(game_night_data[:data][:attributes][:name]).to eq(game_night.name)
