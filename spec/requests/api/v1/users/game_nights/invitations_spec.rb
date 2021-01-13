9ec331666a1d4d179447286d188b89da723a0cb2
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-12 19:11:47 -0700

    Adds sad path test for failed invitation update
    Co-authored-by: Shaun James 
    <58994078+ShaunDaneJames@users.noreply.github.com>

--- spec/requests/api/v1/users/game_nights/invitations_spec.rb
+++ spec/requests/api/v1/users/game_nights/invitations_spec.rb
@@ -88,4 +88,47 @@ RSpec.describe 'Invitations' do
 
     expect(game_night.confirmed_attendees).to eq([user1])
   end
+
+  it 'responds with error message if update fails' do
+    user1 = create :user
+    user2 = create :user
+
+    Friend.create(user_id: user1.id, bud_id: user2.id)
+    Friend.create(user_id: user2.id, bud_id: user1.id)
+
+    game1 = create :game
+
+    UserGame.create(user_id: user1.id, game_id: game1.id)
+
+    game_night = GameNight.create!(
+      user_id: user1.id,
+      name: 'Friday Fun Night',
+      date: '1/15/2021',
+      number_of_games: 2
+    )
+
+    invitation = Invitation.create!(
+      status: 'pending',
+      user_id: user2.id,
+      game_night_id: game_night.id
+    )
+
+    invite_data = {
+      status: 'pending',
+      user_id: user2.id,
+      game_night_id: game_night.id
+    }
+
+    expect(game_night.confirmed_attendees).to eq([user1])
+
+    patch "/api/v1/invitations/#{invitation.id}", params: invite_data
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq(422)
+
+    expect(user2.invitations.first.status).to eq('pending')
+
+    expect(game_night.confirmed_attendees).to eq([user1])
+  end
 end
