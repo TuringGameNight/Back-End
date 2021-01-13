fadefd7bf165180ea7068791ac19eeb92cd0cf23
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-13 16:38:20 -0600

    Add sad path testing for game nights delete action

--- spec/requests/api/v1/game_nights/delete_spec.rb
+++ spec/requests/api/v1/game_nights/delete_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe 'Update Game Night' do
 
     expect(GameNight.all).to eq([])
   end
+
+  it 'returns josn error if not found' do
+    delete api_v1_game_night_path(80_000)
+
+    json_body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq(404)
+    expect(json_body).to have_key(:message)
+    expect(json_body[:message]).to eq('unsuccessful')
+    expect(json_body).to have_key(:error)
+    expect(json_body[:error]).to eq("Couldn't find GameNight with 'id'=80000")
+  end
 end
