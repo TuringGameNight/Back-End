1681b0d45274482aa4e5bf43e4bed090b15ad542
GregJMitchell <g2mitchell3003@gmail.com>
2021-01-12 17:25:53 -0700

    Change creation to use JSON instead of params
    Co-authored-by: Austin Aspaas <61166348+evilaspaas1@users.noreply.github.com>

--- app/controllers/api/v1/game_nights_controller.rb
+++ app/controllers/api/v1/game_nights_controller.rb
@@ -5,34 +5,28 @@ class Api::V1::GameNightsController < ApplicationController
     render json: GameNightSerializer.new(game_night)
   end
 
-  # def create
-  #   game_night_data = JSON.parse(request.body.read, symbolize_names: true)
-  #   game_night_params = game_night_data.extract!(:name, :user_id, :date, :number_of_games)
-  #   # how can we do stronger params here?
-  #   game_night = GameNight.new(game_night_params)
-
-  #   if game_night.save && game_night_data[:friends].count > 0
-  #     game_night_data[:friends].each do |friend_id|
-  #       Invitation.create!(game_night_id: game_night.id, user_id: friend_id, status: 'pending')
-  #     end
-  #     render json: { message: 'success' }, status: :created
-  #   else
-  #     # render json: { error: 'data cannot be blank' }, status: :unprocessable_entity
-  #     render_unprocessable_entity_response(exception)
-  #   end
-  # end
-
   def create
-    game_night = GameNight.new(game_night_params)
+    json = JSON.parse(request.body.string, symbolize_names: true)
 
-    if game_night.save && params[:friends].count.positive?
-      params[:friends].each do |friend_id|
+    game_night = GameNight.new(user_id: json[:user_id],
+                               date: json[:date],
+                               number_of_games: json[:number_of_games],
+                               name: json[:name])
+    json[:friends].delete_at(0)
+    # game_night_data = JSON.parse(request.body.read, symbolize_names: true)
+    # game_night_params = game_night_data.extract!(:name, :user_id, :date, :number_of_games)
+    # # how can we do stronger params here?
+    # game_night = GameNight.new(game_night_params)
+
+    if game_night.save && json[:friends].count.positive?
+      json[:friends].each do |friend_id|
         Invitation.create!(game_night_id: game_night.id, user_id: friend_id, status: 'pending')
       end
 
       render json: { message: 'success' }, status: :created
     else
-      render json: { message: 'unsuccessful', error: 'Could not create game night' }, status: 422
+      render json: { message: 'unsuccessful', error: game_night.errors.full_messages.to_sentence.to_s },
+             status: :unprocessable_entity
     end
   end
 
