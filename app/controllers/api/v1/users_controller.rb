2b0977ca15183149aac7be25a62e1ca3709932c8
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-13 15:27:48 -0700

    Edits users controller to use new name of lookup/create method, adds sad path for failed lookup or create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,8 +5,12 @@ module Api
     class UsersController < ApplicationController
       def create
         user_params = JSON.parse(request.body.read, symbolize_names: true)
-        user = User.find_or_update(user_params)
-        render json: UserSerializer.new(user)
+        user = User.find_or_create(user_params[:user_data])
+        if user.save
+          render json: UserSerializer.new(user)
+        else
+          render json: { message: 'unsuccessful', error: 'Could not find or create user' }, status: :unprocessable_entity
+        end
       end
     end
   end
