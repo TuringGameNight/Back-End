8c0350b368123ff08dd2ddf46601942921a286a3
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-13 15:31:33 -0700

    Updates users create spec to reflect user table changes

--- spec/requests/api/v1/users/create_spec.rb
+++ spec/requests/api/v1/users/create_spec.rb
@@ -5,13 +5,17 @@ require 'rails_helper'
 RSpec.describe 'Create User' do
   it 'responds with user info if user already exists in db' do
     user = create(:user)
-    user_params = { client_id: user.id,
-                    user_data: { info: { name: user.name,
+    user_params = { user_data: { uid: user.google_id,
+                                 info: {
+                                        name: user.name,
                                          email: user.email,
-                                         image: user.image },
-                                 credentials: { token: user.google_token,
-                                                refresh_token: user.refresh_token },
-                                 extra: { id_token: user.auth_token } } }
+                                         image: user.image
+                                       },
+                                        credentials: {
+                                                token: user.auth_token,
+                                                refresh_token: user.refresh_token,
+                                                expires_at: user.token_expiration
+                                                } } }
     headers = { 'CONTENT_TYPE' => 'application/json' }
 
     post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
@@ -23,12 +27,13 @@ RSpec.describe 'Create User' do
 
   it 'responds with new user info if user does not exist in db' do
     user_params = { client_id: '101010',
-                    user_data: { info: { name: 'Elon Musk',
+                    user_data: { uid: '121212121',
+                                 info: { name: 'Elon Musk',
                                          email: 'elonistheman@email.com',
                                          image: 'elonbeingcoolerthanyou.imgur.com' },
                                  credentials: { token: '0101010101',
-                                                refresh_token: '1010101010101' },
-                                 extra: { id_token: '101010101010101010' } } }
+                                                refresh_token: '1010101010101',
+                                                expires_at: 123123123 } } }
     headers = { 'CONTENT_TYPE' => 'application/json' }
 
     post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
