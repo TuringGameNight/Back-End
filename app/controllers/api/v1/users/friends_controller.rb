ce6a9c91ef69bc664f0888dba84d88cf9b155986
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-12 17:02:16 -0700

    Update friends controller update action with correct queries
    Co-authored-by: Grant Dempsey 34411972+GDemps@users.noreply.github.com

--- app/controllers/api/v1/users/friends_controller.rb
+++ app/controllers/api/v1/users/friends_controller.rb
@@ -10,11 +10,13 @@ module Api
         end
 
         def update
-          friend = Friend.find_by(user_id: params["data"][0]["id"], bud_id: params["id"])
+          require "pry"; binding.pry
+          friend = Friend.find_by(user_id: params["user_id"], bud_id: params["friend_id"])
           friend.status = "accepted"
           friend.save
-          results = User.joins(:friends).where("friends.user_id": params[:user_id]).select("users.name, users.email, friends.status")
-          render json: FriendsSerializer.new(results)
+
+          user = User.find(params["user_id"])
+          render json: FriendsSerializer.new(user)
         end
       end
     end
