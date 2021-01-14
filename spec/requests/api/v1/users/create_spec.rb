b68b8d34f0827e95b6026762e5534eea68f03941
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-13 20:12:05 -0700

    moves test to new file
    Co-authored-by: Austin Aspaas 
    <61166348+evilaspaas1@users.noreply.github.com>

--- spec/requests/api/v1/users/create_spec.rb
+++ spec/requests/api/v1/users/create_spec.rb
@@ -3,28 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Create User' do
-  it 'responds with user info if user already exists in db' do
-    user = create(:user)
-    user_params = { user_data: { uid: user.google_id,
-                                 info: {
-                                   name: user.name,
-                                   email: user.email,
-                                   image: user.image
-                                 },
-                                 credentials: {
-                                   token: user.auth_token,
-                                   refresh_token: user.refresh_token,
-                                   expires_at: user.token_expiration
-                                 } } }
-    headers = { 'CONTENT_TYPE' => 'application/json' }
-
-    post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
-
-    user = JSON.parse(response.body, symbolize_names: true)
-    expect(response.status).to eq(200)
-    user_response_checker(user, User.last)
-  end
-
   it 'responds with new user info if user does not exist in db' do
     user_params = { client_id: '101010',
                     user_data: { uid: '121212121',
