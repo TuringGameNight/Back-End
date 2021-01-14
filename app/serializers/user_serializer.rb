6c9177a7fe3b85b1ca7f9aa8e1279c53585885b4
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-13 20:12:37 -0700

    Removes useless associated objects attributes from user serializer
    Co-authored-by: Austin Aspaas 
    <61166348+evilaspaas1@users.noreply.github.com>

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,25 +3,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :email, :image
-
-  attribute :buds do |user|
-    user.buds.map do |bud|
-      {
-        name: bud.name,
-        email: bud.email
-      }
-    end
-  end
-
-  attribute :games do |user|
-    user.games.each do |game|
-      game
-    end
-  end
-
-  attribute :game_nights do |user|
-    user.game_nights.each do |game_night|
-      game_night
-    end
-  end
 end
