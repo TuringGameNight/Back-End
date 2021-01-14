3c1dc2e4168bf251f17bf94ae4c2e6b6578b2cdb
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-13 18:16:48 -0700

    Refactor friends controller update action to use facade

--- app/controllers/api/v1/users/friends_controller.rb
+++ app/controllers/api/v1/users/friends_controller.rb
@@ -10,12 +10,7 @@ module Api
         end
 
         def update
-          friend = Friend.find_by(user_id: params['user_id'], bud_id: params['friend_id'])
-          friend.status = 'accepted'
-          friend.save
-
-          user = User.find(params['user_id'])
-          render json: FriendsSerializer.new(user)
+          render json: FriendsFacade.accept_friendship(params[:user_id], params[:friend_id])
         end
 
         def create
