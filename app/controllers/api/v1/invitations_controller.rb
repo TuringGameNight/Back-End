8892915cf56f9f03ec6ce9a73f610f37974642ae
Grant Dempsey <CryptoDemps@gmail.com>
2021-01-14 20:17:43 -0700

    Add parse to gain access to response.body in update method
    Co-authored-by: Austin Aspaas 
    <61166348+evilaspaas1@users.noreply.github.com>
    Co-authored-by: Phil DeFraties 
    <65036872+PhilipDeFraties@users.noreply.github.com>

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -4,6 +4,7 @@ module Api
   module V1
     class InvitationsController < ApplicationController
       def update
+        invitation_params = JSON.parse(request.body.read, symbolize_names: true)
         Invitation.find_update(invitation_params)
         if Invitation.find_by(id: invitation_params[:id].to_i).status == 'pending'
           render json: { message: 'unsuccessful', error: 'Could not update invitation status' },
